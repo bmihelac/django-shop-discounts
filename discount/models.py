c03459335de43318bec71f4b4cf9c95ac194b085
Bojan Mihelac <bmihelac@mihelac.org>
2012-03-27 21:54:40 +0200

    Move managers to own module.

--- discount/models.py
+++ discount/models.py
@@ -1,30 +1,15 @@
 from datetime import datetime
 
 from django.db import models
-from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
-from polymorphic.manager import PolymorphicManager
 from polymorphic.polymorphic_model import PolymorphicModel
 
 from shop.util.loader import get_model_string
 from shop.models.productmodel import Product
 from shop.cart.cart_modifiers_base import BaseCartModifier
 
-
-class DiscountBaseManager(PolymorphicManager):
-    """
-    A manager for ``DiscountBase`` filtering.
-    """
-
-    def active(self, at_datetime=None, code=''):
-        if not at_datetime:
-            at_datetime = datetime.now
-        qs = self.filter(Q(is_active=True) & 
-                Q(valid_from__lte=at_datetime) & 
-                (Q(valid_until__isnull=True) | Q(valid_until__gt=at_datetime)))
-        qs = qs.filter(Q(code='') | Q(code=code))
-        return qs
+from .managers import DiscountBaseManager
 
 
 class DiscountBase(PolymorphicModel, BaseCartModifier):
