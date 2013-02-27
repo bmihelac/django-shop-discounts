52ae5ed61066c0c7d0f7ca54d015d8c04c4e7691
Bojan Mihelac <bmihelac@mihelac.org>
2013-02-27 11:28:58 +0100

    Make discount code views redirection more configurable.
    Use ``success_url`` class attribute.

--- discount/views.py
+++ discount/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.edit import CreateView
 from django.views.generic import View
 from django.shortcuts import redirect
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 
 from shop.util.cart import get_or_create_cart
 
@@ -13,9 +13,7 @@ from models import CartDiscountCode
 
 class CartDiscountCodeCreateView(CreateView):
     model = CartDiscountCode
-
-    def get_success_url(self):
-        return reverse('checkout_selection')
+    success_url = reverse_lazy('checkout_selection')
 
     def get_form_kwargs(self):
         kwargs = super(CartDiscountCodeCreateView, self).get_form_kwargs()
@@ -26,9 +24,10 @@ class CartDiscountCodeCreateView(CreateView):
 
 
 class CartDiscountCodeDeleteView(View):
+    success_url = reverse_lazy('checkout_selection')
 
     def get_success_url(self):
-        return reverse('checkout_selection')
+        return self.success_url
 
     def post(self, *args, **kwargs):
         cart = get_or_create_cart(self.request)
