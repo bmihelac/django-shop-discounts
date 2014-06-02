673bb8475cee52a7a25785b20fd90a1db8f16b02
Bojan Mihelac <bmihelac@mihelac.org>
2014-06-02 11:06:45 +0200

    If code is present that is only active discount

--- discount/managers.py
+++ discount/managers.py
@@ -13,8 +13,9 @@ class DiscountBaseManager(PolymorphicManager):
     def active(self, at_datetime=None, code=''):
         if not at_datetime:
             at_datetime = datetime.now
-        qs = self.filter(Q(is_active=True) & 
-                Q(valid_from__lte=at_datetime) & 
-                (Q(valid_until__isnull=True) | Q(valid_until__gt=at_datetime)))
-        qs = qs.filter(Q(code='') | Q(code=code))
+        qs = self.filter(Q(is_active=True) &
+                         Q(valid_from__lte=at_datetime) &
+                         (Q(valid_until__isnull=True) |
+                          Q(valid_until__gt=at_datetime)))
+        qs = qs.filter(code=code)
         return qs
